5d6cad86ed1f2a1422b519e2ac5a26467b53e474
adeputragea <adeputragea28@gmail.com>
2022-04-30 17:03:36 +0700

    install react-router-dom, create directory Router and index.js in directory config

---
+++ src/config/Router/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+const Routes = () => {
+  return;
+  <Router>
+    <switch>
+      <Route></Route>
+    </switch>
+  </Router>;
+};
+
+export default Routes;
+
+// tag Router gunanya untuk membungkus semua route
+// di import dari react router dom, untuk mengatur perpindahan
